055566ca9eab8d0ff4afef2d530e00be519a7ac3
Danila <danilaeremenko@mail.ru>
2020-01-29 13:48:53 +0300

    WmtsServer: minimal version intercating with geoviews client

--- Server.py
+++ Server.py
@@ -1,24 +1,30 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from PIL import Image
 import signal
 import sys
 import json
 import pandas as pd
+import numpy as np
 
+img = open('Datasets/Cats/1.jpg', 'rb')
+img_bytes = img.read()
 df = {}
 
 
 class HttpProcessor(BaseHTTPRequestHandler):
     def do_GET(self):
+        global df
         self._send_wmts_response(
             errorCode=200,
             msg="WmtsServer: I got your request server got you request, but I'm still not implemented",
-            data={})
+            data=img_bytes)
+        print("GET Request got")
 
     def _send_wmts_response(self, errorCode, msg, data):
         self.send_response(200)
-        self.send_header('content-type', 'text/json')
+        self.send_header('content-type', 'image/png')
         self.end_headers()
-        self.wfile.write(json.dumps({"errorCode": errorCode, "msg": msg, "data": data}).encode())
+        self.wfile.write(data)
 
 
 def signal_handler(sig, frame):
@@ -31,9 +37,9 @@ def get_df(tile_dir):
     import os
     from PIL import Image
 
-    pref = os.path.abspath(tile_dir)
+    tile_dir_abs = os.path.abspath(tile_dir)
 
-    img_names = os.listdir(pref)
+    img_names = os.listdir(tile_dir_abs)
     img_names.sort()
 
     latitudes = []
@@ -42,7 +48,7 @@ def get_df(tile_dir):
     ids = []
 
     for id, img_name in enumerate(img_names):
-        img_gps = ep.get_gps_info(Image.open("%s/%s" % (pref, img_name)))
+        img_gps = ep.get_gps_info(Image.open("%s/%s" % (tile_dir_abs, img_name)))
         ids.append(id)
         latitudes.append(img_gps["Latitude"])
         longitudes.append(img_gps["Longitude"])
@@ -70,7 +76,7 @@ def main():
     print("SIGINT handler created")
 
     serv = HTTPServer(('', config_dict['PORT']), HttpProcessor)
-    print("Requests expected on %d port\nRunning server..." % config_dict['PORT'])
+    print("Requests expected on %d port\nWMTS server running..." % config_dict['PORT'])
 
     serv.serve_forever()
 
